869053c41973da6c064ebf1c91de79fcb18bf66e
sailakshmidurga <147791796+sailakshmidurga@users.noreply.github.com>
2024-11-04 10:20:37 +0530

    Uploading code: Temperature Conversion.py

---
+++ Temperature Conversion.py
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Temp
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int c=sc.nextInt();
+    float f=c*1.8F+32;
+    System.out.printf("%.2f",f);
+ }
+}
